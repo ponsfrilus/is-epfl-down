abdb18b48b1592566df7b9ca278a17259ebf0349
William Belle <william.belle@gmail.com>
2017-03-25 20:26:01 +0100

    first draft

--- src/cli.js
+++ src/cli.js
@@ -1,3 +1,5 @@
+#!/usr/bin/env node
+
 /*
  * (c) All rights reserved. ECOLE POLYTECHNIQUE FEDERALE DE LAUSANNE,
  * Switzerland, VPSI, 2017.
@@ -5,3 +7,44 @@
  */
 
 'use strict';
+
+var got        = require('got');
+var domains    = require('./domain.json');
+var logSymbols = require('log-symbols');
+var isDown     = false;
+var promises   = [];
+
+var upMsg = function(domain) {
+  console.log(logSymbols.success, domain);
+};
+
+var downMsg = function(domain) {
+  isDown = true;
+  console.log(logSymbols.error, domain);
+};
+
+var finalMsg = function() {
+  if (isDown) {
+    console.log('\n🍺  It\'s time for a break');
+  } else {
+    console.log('\n🦄  Everything is working fine');
+  }
+};
+
+var testDomain = function(domain) {
+  promises.push(got.head(domain).then(function() {
+    upMsg(domain);
+  }).catch(function() {
+    downMsg(domain);
+  }));
+};
+
+for (var i = 0; i < domains.length; i++) {
+  testDomain(domains[i]);
+}
+
+Promise.all(promises).then(function() {
+  finalMsg();
+}).catch(function(e) {
+  console.log(e);
+});
--- test/test.js
+++ test/test.js
@@ -5,3 +5,25 @@
  */
 
 'use strict';
+
+var should = require('chai').should();
+
+describe('cli.js should return result', function() {
+  this.timeout(15000);
+  var response;
+
+  before(function(done) {
+    var execFile = require('child_process').execFile;
+    execFile('./src/cli.js', function(error, stdout) {
+      if (error) {
+        throw error;
+      }
+      response = stdout;
+      done();
+    });
+  });
+
+  it('result should match "time for a break" or "working fine"', function() {
+    response.should.match(/time for a break|working fine/);
+  });
+});
